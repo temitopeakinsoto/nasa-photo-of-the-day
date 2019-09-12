b86ab1b47942eff1eff3be3e95e9065fcfa206fe
Adetunji Awobajo <surfsamson@gmail.com>
2019-09-12 00:58:36 -0700

    feat: Create image component
    In this commit, I created a new component for rendering/display images
    passed into it. The created image component is exported from the image.js
    file and imported into the App..js parent component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "./image";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+
+      <Image />
     </div>
   );
 }
---
+++ src/image.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Image () {
+    return(
+        <div>Hello</div>
+    );
+}
+
+export default Image;
